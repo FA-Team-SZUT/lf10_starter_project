e7a620db16a9781df3090585b7558171b4d88940
mohammad akbari <mohammad.akbari@hmmh.de>
2024-01-24 14:05:00 +0100

    added edit and delete button

--- src/app/qualifications/qualifications.component.ts
+++ src/app/qualifications/qualifications.component.ts
@@ -12,9 +12,37 @@ import { Observable } from 'rxjs';
   styleUrl: './qualifications.component.css',
 })
 export class QualificationsComponent {
-  qualifications$: Observable<Qualification[]> = new Observable();
+  skillProp = '';
+  idProp: number = -1;
   constructor(private reqService: DataRequest) {}
+  qualifications$: Observable<Qualification[]> = new Observable();
+
   ngOnInit() {
     this.qualifications$ = this.reqService.getQualifications();
+  }
+
+  handleProps(id: number | undefined, skill: string | undefined) {
+    if (id && skill) {
+      this.idProp = id;
+      this.skillProp = skill;
+    }
+  }
+  handleSave(id: number | undefined, skill: string) {
+    if (id) {
+      this.reqService.updateQualification(id, skill).subscribe(() => {
+        // refresh the qualifications$ observable
+        this.qualifications$ = this.reqService.getQualifications();
+      });
+    }
+  }
+
+  deleteQualification(id: number | undefined, skillSet: string | undefined) {
+    if (id) {
+      this.reqService
+        .handleDeleteOfQualification(id, skillSet ?? '')
+        .subscribe(
+          () => (this.qualifications$ = this.reqService.getQualifications())
+        );
+    }
   }
 }
